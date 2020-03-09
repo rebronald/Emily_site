76cdb6c375ad6e27dd770f537b4441e26319d113
RR <53237734+rebronald@users.noreply.github.com>
2020-03-10 12:04:30 +1300

    testing if calling it index fixes it

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php include "top_bit.php"; ?>
+
+		<main class="box"> <!--  Main Content -->
+			<h2>Alberton House</h2>
+			<p>
+				Alberton house is a popular heritage home that holds a lot of history and knowledge about the lives of a wealthy colonial family. 
+			</p>    
+			<p>
+				It has a beautiful white, blue and green colour scheme with lots of trees surrounding it. There are tours and exhibitions and you can hire it as a venue for a wedding or special event. 
+			</p>
+			
+			<!-- Image Grid -->
+			<div class="homeimages"> 
+			
+				<!-- Image that links to home page-->
+				<div class="albertonhouse"> 
+					<a href="index_alberton.php">
+						<img class="allround" src="images/alberton_house_op.jpg" alt="Image of front of alberton house. Links to Home Page" width="250" height="250" title="links to home">
+					</a>
+				</div>
+				
+				<!-- Image that links to cost page -->
+				<div class="ballroom"> 
+					<a href="alberton_cost_opentimes.php">
+						<img class="allround" src="images/ball_room_op.jpg" alt="Image of large room with red carpet and big windows. Links to Costs Page" width="250" height="250" title="links to cost page" />
+					</a>
+				</div>
+				
+				<!-- Image that links to events page -->
+				<div class="diningtable">
+					<a href="alberton_events.php"> 
+						<img class="allround" src="images/dining_table_op.jpg" alt="Image of close up of old fashioned dining table with plates and cutlery. Links to Events Page" width="250" height="250" title="links to events page" />
+					</a>
+				</div>
+				
+				<!-- Image that links to gallery -->
+				<div class="prettytrees">
+					<a href="gallery.php"> 
+						<img class="allround" src="images/pretty_trees_op.jpg" alt="Image of green trees with sunlight streaming through. Links to Gallery." width="250" height="250" title="links to gallery" />
+					</a>
+				</div>
+				
+			</div>
+			
+
+		</main>
+		
+		<!-- Aside content -->
+		<aside class="box"> 
+			<h2>For more info:</h2>
+			
+			<ul>
+				<li> <a href="http://www.heritage.org.nz/places/places-to-visit/auckland-region/alberton"> Heritage page on Alberton House </a></li>
+				<li> Contact: 
+					<ul>
+						<li>+64 9 846 7367</li>
+						<li><b>Email:</b></li>
+						<li>alberton@heritage.org.nz</li>
+					</ul>
+				</li>
+			</ul>
+		
+		</aside>
+		
+<?php include "bottom_bit.php"; ?>
+
